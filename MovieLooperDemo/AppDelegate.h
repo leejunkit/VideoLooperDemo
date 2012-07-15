772c365d516aecf347c746fd73a4eff3e824d5a5
Lee Jun Kit <mohawk.riceball@gmail.com>
2012-07-15 10:00:03 +0800

    Initial Commit

---
+++ MovieLooperDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieLooperDemo
+//
+//  Created by Jun Kit Lee on 15/7/12.
+//  Copyright (c) 2012 mohawk.riceball@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MovieLooperDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MovieLooperDemo
+//
+//  Created by Jun Kit Lee on 15/7/12.
+//  Copyright (c) 2012 mohawk.riceball@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
